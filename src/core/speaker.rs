0461d6a661261a665e3beed77e68cbe0ab2dabbe
谦兰君 <hewun@lyceum.cc>
2025-09-09 14:39:38 +0800

    feat(speaker): 使用线程和通道优化音频播放
    重构 Speaker 结构体，将音频播放逻辑移至独立线程并通过通道通信。
    移除直接播放逻辑，改为通过发送消息触发播放，提高响应性能。

--- src/core/speaker.rs
+++ src/core/speaker.rs
@@ -4,27 +4,41 @@ use rodio::{OutputStream, OutputStreamBuilder};
 use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
+use std::sync::mpsc::Sender;
 
 pub struct Speaker {
-    stream_handle: OutputStream,
+    tx: Sender<String>,
 }
 
 impl Speaker {
     pub fn new() -> Result<Self> {
         let stream_handle =
             OutputStreamBuilder::open_default_stream().context("open default stream failed")?;
+        let tx = Self::init_thread(stream_handle);
 
-        Ok(Self { stream_handle })
+        Ok(Self { tx })
+    }
+
+    fn init_thread(stream_handle: OutputStream) -> Sender<String> {
+        let (tx, rx) = std::sync::mpsc::channel::<String>();
+        std::thread::spawn(move || -> Result<()> {
+            while let Ok(audio_path) = rx.recv() {
+                let file = BufReader::new(File::open(&audio_path).context("open file failed")?);
+                info!("play audio: {}", &audio_path);
+                let sink = rodio::play(stream_handle.mixer(), file).context("play wav failed")?;
+                sink.set_volume(1.7);
+                sink.set_speed(1.05);
+                sink.sleep_until_end();
+            }
+
+            Ok(())
+        });
+        tx
     }
 
     pub fn play_wav(&self, path: &str) -> Result<()> {
         if Path::new(path).exists() {
-            let file = BufReader::new(File::open(path).context("open file failed")?);
-            info!("play audio: {}", path);
-            let sink = rodio::play(&self.stream_handle.mixer(), file).context("play wav failed")?;
-            sink.set_volume(1.7);
-            sink.set_speed(1.05);
-            sink.sleep_until_end();
+            self.tx.send(path.to_string())?;
         } else {
             warn!("audio file not found: {}", path)
         }

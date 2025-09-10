a217a4e831cba5cda58b3dd20a88e7567796f4a0
谦兰君 <hewun@lyceum.cc>
2025-09-10 15:01:53 +0800

    feat(keypress): 添加重新执行键盘宏功能
    添加 RESEND 按键枚举和 spare_stack 字段，用于存储并重新执行上一次键盘宏。当按下 RESEND 键时，会将 spare_stack 中的按键序列复制到 one_stack 以便重新执行。

--- src/core/keypress.rs
+++ src/core/keypress.rs
@@ -1,12 +1,12 @@
 use anyhow::{Result, anyhow};
 use log::info;
 use rdev::{EventType, Key, simulate};
+use serde::{Deserialize, Serialize};
 use std::time::Duration;
 use std::{
     collections::HashMap,
     sync::{Arc, Mutex},
 };
-use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub enum LocalKey {
@@ -14,13 +14,17 @@ pub enum LocalKey {
     DOWN,
     LEFT,
     RIGHT,
+    /// 打开战备页面按键
     OPEN,
+    /// 重新执行上一次键盘宏按键
+    RESEND,
 }
 
 pub struct KeyPresser {
     /// 按键映射
     key_map: Arc<HashMap<LocalKey, Key>>,
     one_stack: Arc<Mutex<Option<Vec<LocalKey>>>>,
+    spare_stack: Arc<Mutex<Option<Vec<LocalKey>>>>,
 }
 
 impl KeyPresser {
@@ -28,17 +32,20 @@ impl KeyPresser {
         Self {
             key_map: Arc::new(key_map),
             one_stack: Arc::new(Mutex::new(None)),
+            spare_stack: Arc::new(Mutex::new(None)),
         }
     }
 
     pub fn push(&self, keys: &[LocalKey]) {
         *self.one_stack.lock().unwrap() = Some(keys.to_vec());
+        *self.spare_stack.lock().unwrap() = Some(keys.to_vec());
     }
 
     /// block
     pub fn listen(&self) -> Result<()> {
         let key_map = Arc::clone(&self.key_map);
         let one_stack = Arc::clone(&self.one_stack);
+        let spare_stack = Arc::clone(&self.spare_stack);
 
         // keypress worker
         let (tx, rx) = std::sync::mpsc::channel::<Vec<LocalKey>>();
@@ -66,6 +73,11 @@ impl KeyPresser {
                 if key == *key_map.get(&LocalKey::OPEN).unwrap() {
                     if let Some(keys) = one_stack.lock().unwrap().take() {
                         tx.send(keys).unwrap();
+                    }
+                } else if key == *key_map.get(&LocalKey::RESEND).unwrap() {
+                    if let Some(keys) = spare_stack.lock().unwrap().clone() {
+                        info!("resend key press: {:?}", &keys);
+                        one_stack.lock().unwrap().replace(keys);
                     }
                 }
             }
